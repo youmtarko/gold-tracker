a89e148913bbf9f817468a61700533785cdfea01
youmtarko <moulhamnakeshbandi1@gmail.com>
2026-05-20 19:57:27 +0200

    remove api route

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+  output: 'export',
+};
 
-export default nextConfig
+export default nextConfig;
